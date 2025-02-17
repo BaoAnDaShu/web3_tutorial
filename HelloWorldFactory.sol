02ea2f379b8173f9d6ea555a7c7e695f968565f9
BaoAnDaShu <v3n0m@qq.com>
2025-02-17 22:14:13 +0800

    no1

---
+++ HelloWorldFactory.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import { HelloWorld } from "./HelloWorld.sol";
+
+contract HelloWorldFactory {
+    HelloWorld hw;
+    function createHelloWorld() public  {
+        hw = new HelloWorld();
+    }
+    
+}
+
